a412cc36997a067bb94c17192118af66906fc7bc
BrnBastos <bruninhosbastos0228@gmail.com>
2022-10-31 09:07:24 -0300

    Carrinhos Classe

---
+++ API_TccGamesWave/Models/Carrinho.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace API_TccGamesWave.Models
+{
+    public class Carrinho
+    {
+        public int CodCarrinho { get; set; }
+        public decimal ValorTotal { get; set; }
+        public string Cupom { get; set; }
+
+    }
+}
---
+++ API_TccGamesWave/Models/ItemCarrinho.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace API_TccGamesWave.Models
+{
+    public class ItemCarrinho
+    {
+        public int CodProd_CodCarrinho { get; set; }
+        public int QtnProd { get; set; }
+        public string ValorUnit { get; set; }
+        public string ValorTotal { get; set; }
+    }
+}
